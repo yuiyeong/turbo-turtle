2f7fc074b269810c965ea5b5f9e428a7a57d5b28
yuiyeong <joyuiyeong@gmail.com>
2025-02-25 21:42:09 +0900

    Feat: django-debug-toolbar 설정

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,5 +1,8 @@
 from .base import *
 
+INTERNAL_IPS = ["127.0.0.1"]
+
 if DEBUG:
+    # django-debug-toolbar 설정
     INSTALLED_APPS += ["debug_toolbar"]
-    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+    MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,14 @@
+import debug_toolbar
+
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
+from config import settings
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(route="admin/", view=admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path(route="__debug__/", view=include(debug_toolbar.urls))]
